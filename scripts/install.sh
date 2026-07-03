5db3eda9b8ecb006433e240ecb2d2866949bc348
veerababu narni <147053314+CloudTechDevOps@users.noreply.github.com>
2026-07-03 16:26:39 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
+mkdir /home/ec2-user/python-app
+
 cd /home/ec2-user/python-app
 
 python3 -m venv venv
 
 source venv/bin/activate
 
-yum install python3-pip -y
-yum install git -y
-pip install -r requirements.txt
+sudo yum install python3-pip -y
+pip3 install -r requirements.txt
