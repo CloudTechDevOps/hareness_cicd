4634f2ffc39aba707388e8df33178ac90f4adcae
veerababu narni <147053314+CloudTechDevOps@users.noreply.github.com>
2026-07-03 16:36:13 +0530

    Update install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,12 +1,14 @@
 #!/bin/bash
+set -e
 
-mkdir /home/ec2-user/python-app
+APP_DIR=/home/ec2-user/python-app
+SRC_DIR=/tmp/mechanical
 
-cd /home/ec2-user/python-app
+mkdir -p "$APP_DIR"
+cp -r "$SRC_DIR"/* "$APP_DIR"/
 
-python3 -m venv venv
-
-source venv/bin/activate
-
+cd "$APP_DIR"
 sudo yum install python3-pip -y
+python3 -m venv venv
+source venv/bin/activate
 pip3 install -r requirements.txt
