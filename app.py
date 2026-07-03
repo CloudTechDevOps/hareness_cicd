37f5edd729bc20c7f7862d0fce4641250991e6cf
veerababu narni <147053314+CloudTechDevOps@users.noreply.github.com>
2026-07-03 18:02:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -12,8 +12,7 @@ import random
 import logging
 from collections import deque
 from datetime import datetime, timezone
-sdfgfdsf
-sdddsds
+
 import psutil
 from flask import Flask, jsonify, render_template
 
